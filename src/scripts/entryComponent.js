319b14ef9b653ece8efcc3db760e24319d5e11a6
Cherkesky <guy@cherkesky.com>
2019-10-30 15:27:37 -0500

    fixed a missing p tag and added labels to the cards

--- src/scripts/entryComponent.js
+++ src/scripts/entryComponent.js
@@ -6,9 +6,12 @@ const entryManager = {
       <section class="card">
         <h3 class="card-header">${journalEntry.concept}</h3>
         <div class="card-body">
-        <p class="card-title">${journalEntry.entry}</p>
-        <p class="card-text"${journalEntry.mood}</p>
-        <p class="card-text">${journalEntry.date}</p>
+
+        <p class="card-title"> We covered: ${journalEntry.entry}</p>
+        <p class="card-text"> My mood was: ${journalEntry.mood}</p>
+        <p class="card-text">Date: ${journalEntry.date}</p>
+
+        
         </div>
         <hr>
       </section>

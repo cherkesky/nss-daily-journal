b0626291dbe5b7720bf413a117f996d4518e11a7
Cherkesky <guy@cherkesky.com>
2019-10-30 15:01:11 -0500

    more validation

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -21,9 +21,9 @@ document.getElementById("record").addEventListener("click", function () {
     let entryValue = document.getElementById("entry-input").value
     let moodValue = document.getElementById("mood-input").value
 
-    const regex = /[^A-Za-z0-9;:{}()\s]+/g
-    if (conceptValue.match(regex) || entryValue.match(regex)) {
-        window.alert("CONTAINS FORBIDDEN CHARACTERS")
+    const regex = /[^A-Za-z0-9;:{entryValue}()\s]+/g
+    if (conceptValue.match(regex) || entryValue.match(regex)|| dateValue==="" || entryValue==="" || conceptValue ==="" ) {
+        window.alert("PLEASE REVIEW THE ENTERED INFO")
     } else {
         console.log (dateValue, conceptValue, entryValue, moodValue)
         let journalFactoryObject = entryManager.entriesFactory(dateValue, conceptValue, entryValue, moodValue)

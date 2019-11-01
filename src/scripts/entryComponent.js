dbf3d623dde9e7f0ab907d6456aadf7ffdeecc38
Cherkesky <guy@cherkesky.com>
2019-11-01 13:15:40 -0500

    more comments

--- src/scripts/entryComponent.js
+++ src/scripts/entryComponent.js
@@ -1,7 +1,10 @@
-const entryManager = {
 
+
+const entryManager = {
+//*********************************************************************************************** 
+// Creating HTML structure for a journal entry
+//*********************************************************************************************** 
   makeJournalEntryComponent(journalEntry) {
-    // Creating HTML structure for a journal entry
     return `
       <section class="card">
         <h3 class="card-header">${journalEntry.concept}</h3>
@@ -10,9 +13,9 @@ const entryManager = {
         <p class="card-title"> We covered: ${journalEntry.entry}</p>
         <p class="card-text"> My mood was: ${journalEntry.mood}</p>
         <p class="card-text">Date: ${journalEntry.date}</p>
-
         
         <button type="button" id="edit-button--${journalEntry.id}" class="btn btn-lg btn-light"><i class="far fa-edit"></i> </button>
+        
         <button type="button" id="delete-button--${journalEntry.id}" class="btn btn-lg btn-light"><i class="far fa-trash-alt"> </i> </button>
 
 
@@ -21,8 +24,9 @@ const entryManager = {
       </section>
     `
   },
-
-
+//*********************************************************************************************** 
+// Journal entries factory function
+//*********************************************************************************************** 
   entriesFactory(date, concept, entry, mood) {
     return {
       date: date,
